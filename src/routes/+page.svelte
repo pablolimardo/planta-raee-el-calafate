e4ed597c10a538b4c95b73bd01de8131fb80d798
Pablo Limardo <plimardo@gmail.com>
2023-10-06 17:54:37 -0300

    Subida de Imagenes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,155 +1,35 @@
-<main>
-    <div class="Banner">
-        <div>
-            <img class="logo" src="LOGO-INDUS.png" alt="Logo">
-        </div>
-        <div class="infobar">
-            <a  class ="boton1" href="https://goo.gl/maps/dqvZvKNunUZW1vAY9">Ubicacion</a>
-        </div>
-    </div> 
-    <div>
-    </div>
-      <div class="nota1">
-        <h1 class="letrasN">Proyecto de Gestión Sostenible de Residuos Electrónicos en El Calafate
-        </h1>
-      </div>
-      <div class="contenedor">
-        <div class="columna">
-        </div>
-        
-        <div class="columna">
-           
-        </div>
-    </div>
-    <div class="info">
-      <h3 class="tituloL"><span id=N1>Desafíos Ambientales Globales y Locales</span></h3>
-      <p class="letrasN">La creciente demanda y consumo de Artefactos Eléctricos y Electrónicos (AEE) en las últimas
-        décadas ha generado un problema ambiental a nivel mundial debido a la incorrecta gestión de los
-        desechos que producen. Esta problemática, silenciosa pero cada vez más peligrosa, representa una
-        amenaza diaria en nuestra comunidad. Dada la riqueza ecológica de El Calafate, con tesoros como
-        el Parque Nacional Perito Moreno y el Humedal Calafateño Laguna Nimez y Bahía del Lago
-        Argentino, resulta urgente tomar medidas de conservación ambiental.
-        </p>
-        <h3 class="tituloL"><span id=N2>Importancia de la Conservación Ambiental en El Calafate</span></h3>
-        <p class="letrasN">Nuestra ubicación geográfica resalta la importancia de proteger la biodiversidad y las masas de agua
-          dulce que nos rodean de la contaminación generada por desechos tóxicos como metales pesados y
-          contaminantes orgánicos resistentes (CPOs). Estos elementos, bioacumulables y degradables con
-          dificultad, plantean riesgos significativos para los ecosistemas acuáticos y la salud humana.</p>
-      <h3 class="tituloL"><span Id="N3">Educación y Acción: Abordando la Problemática de los Residuos Electrónicos</span></h3>
-      <p class="letrasN">Mediante un análisis y observación detallados, hemos identificado diversos AEE, desde dispositivos
-        móviles hasta electrodomésticos, que contribuyen a esta problemática. Además, se ha notado una
-        desinformación generalizada en la comunidad respecto al manejo adecuado de estos desechos
-        electrónicos.</p>
-      <h3 class="tituloL"><span Id="N4">Beneficios Duales: Avance Educativo y Reducción del Impacto Ambiental</span>
-      </h3>
-      <p class="letrasN">Nuestro proyecto busca dos objetivos simultáneos. En primer lugar, mejorar la formación y el
-        currículo de los alumnos a través de la incorporación de tecnologías y equipamiento educativo,
-        promoviendo la colaboración con organizaciones del ámbito socio-productivo. En segundo lugar,
-        aspiramos a reducir el impacto ambiental negativo por habitante en El Calafate, abordando de
-        ma nera efectiva la gestión de los desechos electrónicos.
-        </p>
-        <h3 class="tituloL"><span Id="N5">Estrategias Multidisciplinarias para una Gestión Efectiva</span></h3>
-        <p class="letrasN">Las acciones planificadas se desarrollarán en múltiples ámbitos curriculares de nuestra institución,
-          incluyendo la nueva tecnicatura en Automatización y Control de Procesos Industriales (A.C.P.I.).
-          Esta iniciativa representa un desafío y una oportunidad para aplicar conocimientos técnicos y
-          pedagógicos en un enfoque interdisciplinario. La creación de un centro de reciclaje de Residuos de
-          AEE abordará estos nuevos desafíos y se integrará en espacios curriculares y prácticas
-          profesionalizantes.</p>
-          <h3 class="tituloL"><span Id="N6">Impulsando un Cambio Positivo en la Comunidad</span>
-          </h3>
-          <p class="letrasN">En resumen, nuestro proyecto busca comprometer a toda la comunidad educativa de la Escuela
-            Industrial N°9 y de El Calafate en la gestión adecuada de los Residuos de AEE, siguiendo los
-            principios de la Ley de Educación Ambiental Integral 27.621 y la economía circular. Aspiramos a
-            fomentar una actitud proactiva y responsable hacia el consumo y la generación de desechos
-            electrónicos en nuestra localidad. Juntos, podemos generar un cambio positivo y sostenible.</p>
-    </div>
-  </main>
-  <style>
-
-    /*CSS*/
-    .nota1 h1{
-      align-items: center;
-      flex-direction: column;
-    display: inline-block; /* Asegura que la línea se extienda solo hasta el ancho del texto */
-    padding-bottom: 5px;
-    }
-    a{
-      text-decoration: none;
-      
-    }
-  
-    div{
-      justify-content: center;
-    align-content: center;
-    }
-    .Banner {
-        display: flex;
-        align-items: center;
-        height: 70px;
-        border-bottom: 1px solid #333; /* Línea sólida de 3 píxeles de ancho y color negro */
-        width: 100%;
-        z-index: 100;
-    }
-
-    .infobar{
-        margin-right: 10px;
-        margin-left: 10px;
-        
-    }
-    .logo{
-        margin: 10px;
-		width: 3em;
-        height: auto; 
-        
-    }
-
-    main { 
-    padding : 0;
-    margin : 0;
-    text-decoration: none;
-    list-style: none;
-    box-sizing: border-box;
-    font-family:'Poppins', sans-serif;
-      background-image: url(Fondo-RAEE.avif);
-    }
-
-    .nota1{
-      margin-top: 10px;
-      justify-content: center;
-      display: flex;
-      align-items: center;
-      flex-direction: column;
-    }
-
-
-.info{
-  padding-left: 12px;
-      justify-content: center;
-      display: flex;
-      align-items: center;
-      flex-direction: column;
-    justify-content: center;
-    align-content: center;
-}
-
-.tituloL{
-  border-bottom: 1px solid #000; /* Línea sólida de 3 píxeles de ancho y color negro */
-    display: inline-block; /* Asegura que la línea se extienda solo hasta el ancho del texto */
-    padding-bottom: 5px;
-    font-family: Georgia, 'Times New Roman', Times, serif;
-}
-.letrasN{
-    text-align: center;
-    display: flex;
-}
-.infobar .boton1{
-    color: #333;
-    padding : 8px 6px;
-    border: 1px solid #333;
-    transition: all .3s ease-in-out;
-  }
-  .infobar .boton1:hover{
-    background-color: #333;
-    color: #fff
-  }
-  </style>
+<img src="proyecto_RAEE _00001.jpg" alt="">
+<img src="proyecto_RAEE _00002.jpg" alt="">
+<img src="proyecto_RAEE _00003.jpg" alt="">
+<img src="proyecto_RAEE _00004.jpg" alt="">
+<img src="proyecto_RAEE _00005.jpg" alt="">
+<img src="proyecto_RAEE _00005.jpg" alt="">
+<img src="proyecto_RAEE _00006.jpg" alt="">
+<img src="proyecto_RAEE _00007.jpg" alt="">
+<img src="proyecto_RAEE _00008.jpg" alt="">
+<img src="proyecto_RAEE _00009.jpg" alt="">
+<img src="proyecto_RAEE _00010.jpg" alt="">
+<img src="proyecto_RAEE _00011.jpg" alt="">
+<img src="proyecto_RAEE _00012.jpg" alt="">
+<img src="proyecto_RAEE _00013.jpg" alt="">
+<img src="proyecto_RAEE _00014.jpg" alt="">
+<img src="proyecto_RAEE _00015.jpg" alt="">
+<img src="proyecto_RAEE _00016.jpg" alt="">
+<img src="proyecto_RAEE _00017.jpg" alt=""><img src="proyecto_RAEE _00001.jpg" alt="">
+<img src="proyecto_RAEE _00002.jpg" alt="">
+<img src="proyecto_RAEE _00003.jpg" alt="">
+<img src="proyecto_RAEE _00004.jpg" alt="">
+<img src="proyecto_RAEE _00005.jpg" alt="">
+<img src="proyecto_RAEE _00005.jpg" alt="">
+<img src="proyecto_RAEE _00006.jpg" alt="">
+<img src="proyecto_RAEE _00007.jpg" alt="">
+<img src="proyecto_RAEE _00008.jpg" alt="">
+<img src="proyecto_RAEE _00009.jpg" alt="">
+<img src="proyecto_RAEE _00010.jpg" alt="">
+<img src="proyecto_RAEE _00011.jpg" alt="">
+<img src="proyecto_RAEE _00012.jpg" alt="">
+<img src="proyecto_RAEE _00013.jpg" alt="">
+<img src="proyecto_RAEE _00014.jpg" alt="">
+<img src="proyecto_RAEE _00015.jpg" alt="">
+<img src="proyecto_RAEE _00016.jpg" alt="">
+<img src="proyecto_RAEE _00017.jpg" alt="">
