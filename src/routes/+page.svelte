d8de979db478b5d5c1cfd5d79f69b9f37142d84c
Pablo Limardo <plimardo@gmail.com>
2023-11-20 14:55:47 -0300

    Agrego video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
+<svelte:head>
+	<title>Inicio</title>
+	<meta name="description" content="Bienvenidos al proyecto de planta RAEE de El calafate" />
+</svelte:head>
+
 <img src="proyecto_RAEE _00001.jpg" alt="">
+<div class="video">
+    <h2 class="tituloDoc">Documental sobre residuos electrónicos</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/25uIBlSVrnU?si=RqKuNTrw94nrqYeV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</div>
 <img src="proyecto_RAEE _00002.jpg" alt="">
 <img src="proyecto_RAEE _00003.jpg" alt="">
 <img src="proyecto_RAEE _00004.jpg" alt="">
@@ -34,3 +43,17 @@
 <img src="proyecto_RAEE _00015.jpg" alt="">
 <img src="proyecto_RAEE _00016.jpg" alt="">
 <img src="proyecto_RAEE _00017.jpg" alt="">
+
+<style>
+    .video{
+        display: grid;
+        justify-content: center;
+        gap: 1em;
+        padding: 2em 0;
+    }
+    .tituloDoc{
+        text-align: center;
+        text-transform: uppercase;
+        font-size: 1.5em;
+    }
+</style>
